b5b8796c04cca5de01322e4be700951381b36a86
Denis <zhuravlev_denis01@mail.ru>
2021-08-11 19:05:33 +0300

    connected script modal window and script fslightbox

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ document.querySelector('.btn-burger').addEventListener('click', (event) => {
     document.querySelector('.section-header').classList.toggle('section-header--active-nav')
 })
 // === / Подключаем бургер меню  ===
+
+// === Модульное окно ===
+let button = document.getElementById('button')
+let close = document.getElementById('close')
+let modal = document.getElementById('modal')
+
+button.addEventListener('click', function(event) {
+    event.preventDefault()
+    modal.style.display = "block"
+})
+
+close.addEventListener('click', function(event) {
+    event.preventDefault()
+    modal.style.display = "none"
+})   
+
+// === / Модульное окно ===
